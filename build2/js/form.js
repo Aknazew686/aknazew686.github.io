e8870dc0b34bf4fb70fc3534a4fd049eafbf01dc
Aknazew686 <Aknazew686@gmail.com>
2020-08-04 16:47:44 +0300

    вносит изменения

---
+++ build2/js/form.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var lastName = document.querySelector("[name=last-name]");
+var firstName = document.querySelector("[name=first-name]");
+var phone = document.querySelector("[name=phone]");
+var email = document.querySelector("[name=email]");
+var form = document.querySelector("form");
+var popupSend = document.querySelector('.popup--send');
+var popupButtons = document.querySelectorAll('.popup__button')
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  popupSend.classList.add('popup-show')
+});
+
+for (let popupButton of popupButtons) {
+  popupButton.addEventListener('click', function () {
+    popupSend.classList.remove('popup-show')
+    })
+  }
