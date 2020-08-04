5b7730a4a583cc535f031de74704182632e1119c
Aknazew686 <Aknazew686@gmail.com>
2020-08-04 17:16:54 +0300

    вносит изменения

--- build3/js/main.js
+++ build3/js/main.js
@@ -3,14 +3,17 @@
   var button = document.querySelector('.adress-block-button');
   var popup = document.querySelector('.popup');
   var closePopup = document.querySelector('.close');
+  var form = document.querySelector('form')
+  var firstName = document.querySelector("[name=first-name]");
+
 
   button.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add('popup-show')
+    firstName.focus();
   });
 
-  closePopup.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  closePopup.addEventListener("click", function () {
     popup.classList.remove('popup-show')
   });
 
@@ -19,4 +22,10 @@
       evt.preventDefault();
       popup.classList.remove("popup-show");
     }
+  });
+
+  form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove('popup-show')
+    form.reset();
   });
